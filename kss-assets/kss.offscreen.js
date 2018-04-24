5c45ec93b5c9d1da551143b596540f3524f62472
Nicholas Fletcher <hi@flxa.co>
2018-04-24 11:17:32 +1000

    Markup, Syntax & Nav changes (#3)
    * Add markup back in, add options to builder for showMarkup, markupClass and highlightSyntax. Add default syntax highlighting (Prism). Add a backdrop for small screens to make closing the nav easier, add initial-width meta to prevent zoomout when menu open (should still allow zooming)
    
    * Update twig file to remove output of duplicate markup file name. Remove footer github fork ribbon. Remove verbose from builder and fix lint issue for dangle, update readme with changes
    
    * Remove the twig filename from the markup toggle and add back in the one under the description. Add the details-element-polyfill js file and referrence from index.twig

--- kss-assets/kss.offscreen.js
+++ kss-assets/kss.offscreen.js
@@ -1,20 +1,29 @@
 var toggle = document.getElementById('kss-offscreen-toggle');
+var backdrop = document.getElementById('kss-offscreen-item-backdrop');
 
 var desktopWidth = 769;
 var toggleOpen = true;
 
 var toggleMenu = function () {
   if (toggleOpen) {
-    document.body.classList.remove('is-moved');
-    toggle.classList.remove('is-active');
-    toggle.setAttribute('aria-expanded', 'false');
+    closeMenu();
   }
   else {
-    document.body.classList.add('is-moved');
-    toggle.classList.add('is-active');
-    toggle.setAttribute('aria-expanded', 'true');
+    openMenu();
   }
   toggleOpen = !toggleOpen;
+};
+
+var openMenu = function () {
+  document.body.classList.add('is-moved');
+  toggle.classList.add('is-active');
+  toggle.setAttribute('aria-expanded', 'true');
+};
+
+var closeMenu = function () {
+  document.body.classList.remove('is-moved');
+  toggle.classList.remove('is-active');
+  toggle.setAttribute('aria-expanded', 'false');
 };
 
 if (window.innerWidth <= desktopWidth) {
@@ -23,5 +32,9 @@ if (window.innerWidth <= desktopWidth) {
 }
 
 toggle.addEventListener('click', function () {
+  toggleMenu();
+});
+
+backdrop.addEventListener('click', function () {
   toggleMenu();
 });
